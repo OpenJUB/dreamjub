1dfc22cc392218d06bdf63fd283be1ff1c2ee63f
bklw <x32000@gmail.com>
2016-09-29 10:59:55 +0200

    Update login form to use a custom login view
    Created custom login view.
    Added CSRF Token to login form.
    Added redirect on login success.

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
-
 from oauth2_provider import urls as oauth_urls
 
+from . import views
+
 urlpatterns = [
-    url(r'^$', auth_views.login, {'template_name': 'login/login.html'}),
+    url(r'^$', views.login),
     url(r'^o/', include(oauth_urls, namespace='oauth2_provider')),
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
+from django.contrib import auth as auth_helpers
+from django.views.decorators import debug
 
-# Create your views here.
+
+@debug.sensitive_post_parameters()
+def login(request, template_name='login/login.html'):
+    if request.method == 'POST':
+        response_data = {}
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth_helpers.authenticate(username=username, password=password)
+        if user is not None:
+            auth_helpers.login(request, user)
+
+            response_data['status'] = 'OK'
+        else:
+            # Return an 'invalid login' error message.
+            response_data['status'] = 'ERROR'
+            response_data['detail'] = 'Your username or password is invalid.'
+
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    if request.method == 'GET':
+        return auth_views.login(request, template_name=template_name)
