60e263716e7fee686aa55c5bc40c92e1a67684c7
giorgi <giorgi.kabanashvili@gmail.com>
2016-10-04 14:10:44 +0200

    Fixing check_pep8.py file
    I excluded dreamjub/migrations and login/migrations from flake8
    to get rid of linting errors.

--- check_pep8.py
+++ check_pep8.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.call(['flake8', '--exclude', 'env', '.'])
+subprocess.call(['flake8', '--exclude', 'env, dreamjub/migrations, login/migrations', '.'])
