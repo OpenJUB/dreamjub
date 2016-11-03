65bdee528c80de41450ee5d19c438a777c22611d
Leonhard Kuboschek <x32000@gmail.com>
2016-11-03 16:50:54 +0100

    Add styled authorization view
    Add authorize.html, using Polymer elements to display authorization dialog.
    Modify URLs.py to exclude most OAuth views.
    Create subclass of AuthorizationView

--- login/views.py
+++ login/views.py
@@ -4,11 +4,12 @@ from django.contrib.auth import views as auth_views
 from django.contrib import auth as auth_helpers
 from django.views.decorators import debug
 
+from oauth2_provider import views as oauth_views
+
 
 @debug.sensitive_post_parameters()
 def login(request, template_name='login/login.html'):
     """
-
     :param request: HTTP Request
     :param template_name: Name of the login template to use
     :return: On POST, returns a JSON object indicating the status of the login.
@@ -34,3 +35,7 @@ def login(request, template_name='login/login.html'):
 
     if request.method == 'GET':
         return auth_views.login(request, template_name=template_name)
+
+
+class AuthorizationView(oauth_views.AuthorizationView):
+    template_name = "login/authorize.html"
