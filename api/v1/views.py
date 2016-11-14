356aa70b19e8ff2e8c9db0c250abeedab9a25ea9
Leonhard Kuboschek <x32000@gmail.com>
2016-11-14 21:14:23 +0100

    Add user API (#30)
    * Add StudentViewSet
    * Add /users API endpoint

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,5 +1,4 @@
-from rest_framework import serializers, views
-from oauth2_provider.ext import rest_framework as oauth_ext
+from rest_framework import serializers, views, viewsets, filters
 from django import shortcuts
 
 from dreamjub import models as core_models
@@ -11,10 +10,26 @@ class StudentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CurrentStudentView(views.APIView):
-    permission_classes = [oauth_ext.TokenHasScope]
-    required_scopes = ['profile']
+class StudentViewSet(viewsets.ReadOnlyModelViewSet):
+    # Permissions
+    required_scopes = []
 
+    # Content
+    queryset = core_models.Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # Filtering
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,
+                       filters.SearchFilter)
+    filter_fields = ('college', 'room', 'year', 'majorShort', 'status',
+                     'country', 'active')
+    search_fields = ('username', 'firstName', 'lastName')
+    ordering_fields = ('id', 'eid', 'year', 'majorShort')
+
+    lookup_field = 'username'
+
+
+class CurrentStudentView(views.APIView):
     def get(self, request):
         student = shortcuts.get_object_or_404(core_models.Student,
                                               username=request.user.username)
