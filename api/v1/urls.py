bafe1ea38678bbc7c9a3f1f17549ff7595498719
Leo <x32000@gmail.com>
2016-11-15 13:27:20 +0100

    Add user API (#30) (#31)
    * Add user API (#30)
    
    * Add StudentViewSet
    * Add /users API endpoint
    
    * Add django-filter to requirements.txt

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -8,9 +8,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'users', views.StudentViewSet)
 
 urlpatterns = [
-    urls.url(r'users/me', views.CurrentStudentView.as_view())
+    urls.url(r'users/me', views.CurrentStudentView.as_view()),
 ]
 
 urlpatterns += router.urls
--- dreamjub/settings.py
+++ dreamjub/settings.py
@@ -136,7 +136,14 @@ OAUTH2_PROVIDER = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
-    )
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 
