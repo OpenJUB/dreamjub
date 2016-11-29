1e5191857588cf5bd9e189635291d704cf64b7a0
Tom Wiesing <tkw1536@users.noreply.github.com>
2016-11-29 16:49:49 +0100

    Add more operators to the Query Language (#40)
    * PEP8 compliance
    
    Update several files to be PEP8 compliant
    
    * Update PreJsPy() and QBuilder() for search endpoint
    
    Configure the PreJsPy() instance for the search endpoint to add more
    useful operators and remove a lot of unused ones. The operators
    originally come from Jay.
    
    Implement these operations inside the QBuilder(). Amongst other (minor
    changes) implicitly reduce compound expressions into and-connected clauses.
    
    * Update PreJsPy() instance for search endpoint
    
    Configure the PreJsPy() instance for the search endpoint to add more
    useful operators and remove a lot of unused ones. The operators
    originally come from Jay.
    
    * Add missing operator for "not equal" to QBuilder.

--- api/filters/extended.py
+++ api/filters/extended.py
@@ -10,6 +10,32 @@ class TLFilter(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         parser = PreJsPy.PreJsPy()
+
+        # Do not use the tertiary operator -- we do not need it
+        parser.setTertiaryOperatorEnabled(False)
+
+        # Set up the unary operators properly
+        parser.setUnaryOperators([
+            'not', '!', '~'
+        ])
+
+        parser.setBinaryOperators({
+            # LOGICAL CONNECTIVES
+            'and': 1, '&': 1, '&&': 1, '*': 1,
+            'or': 1, '|': 1, '||': 1, '+': 1,
+            'nand': 1, '!&': 1,
+            'xor': 1, '^': 1,
+
+            # Filters
+            'equals': 2, ':': 2, '=': 2, '==': 2, '===': 2,
+            'less than': 2, '<': 2,
+            'less than or equal': 2, '<=': 2, '=<': 2,
+            'greater than': 2, '>': 2,
+            'greater than or equal': 2, '>=': 2, '=>': 2,
+            'contains': 2, '::': 2,
+            'matches': 2, 'unicorn': 2, '@': 2,
+        })
+
         builder = qb.QBuilder()
 
         query_str = request.GET.get('tl', default=None)
