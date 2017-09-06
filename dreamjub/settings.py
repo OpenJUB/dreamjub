fe82c872019b5395b2264f8e6b37d6a98aecb08a
Tom Wiesing <tkw1536@users.noreply.github.com>
2017-09-06 11:29:48 +0200

    Update OAuth Provider Class Location (#56)
    A recent update to django-oauth-toolkit amongst other things moved the
    Django OAuth Provider Classes from ext.rest_framework to
    contrib.rest_framework. This caused an exception when running dreamjub
    with the newest version of oauth-toolkit.
    
    This commit fixes the problem by updating settings.py to use the
    provider from the new source instead.

--- dreamjub/settings.py
+++ dreamjub/settings.py
@@ -136,7 +136,7 @@ OAUTH2_PROVIDER = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
